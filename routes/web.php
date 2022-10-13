94d433551092fa1d85c73570d2098ae6eed07847
Rafael Schwambach <rafael.schwambach@unidavi.edu.br>
2022-10-13 14:47:42 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContatosController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('contatos/');
+});
+
+
+Route::get('contatos/buscar',[ContatosController::class,'buscar']);
+
+Route::resource('contatos',ContatosController::class);
+
+Route::get('livros/buscar',[LivrosController::class,'buscar']);
+
+Route::resource('livros',LivrosController::class);
